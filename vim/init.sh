fff57d6b02da1bb23c947875253943edf8076f79
Shengkui Leng <lengshengkui@outlook.com>
2017-05-31 15:32:12 +0800

    vim: swith to vim-plug

--- vim/init.sh
+++ vim/init.sh
@@ -5,4 +5,6 @@ VIM_DIR="${HOME}/.vim"
 mkdir -p "${VIM_DIR}"/{bundle,tags,undo}
 cp -a UltiSnips "${VIM_DIR}"
 cp _vimrc "${HOME}"/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git "${VIM_DIR}"/bundle/Vundle.vim
+#git clone https://github.com/VundleVim/Vundle.vim.git "${VIM_DIR}"/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
