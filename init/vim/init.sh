2376f21552d4596a14c469c285ccae3fe4f59a60
Shengkui Leng <lengshengkui@outlook.com>
2017-06-06 14:26:36 +0800

    modify vim inst script

--- init/vim/init.sh
+++ init/vim/init.sh
@@ -6,5 +6,5 @@ mkdir -p "${VIM_DIR}"/{bundle,tags,undo}
 cp -a UltiSnips "${VIM_DIR}"
 cp _vimrc "${HOME}"/.vimrc
 #git clone https://github.com/VundleVim/Vundle.vim.git "${VIM_DIR}"/bundle/Vundle.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo "${VIM_DIR}"/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
