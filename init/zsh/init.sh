e32ee68d643ca80db01874094dd9039cd52058a4
Shengkui Leng <lengshengkui@outlook.com>
2017-06-06 15:10:11 +0800

    change installation of oh-my-zsh

--- init/zsh/init.sh
+++ init/zsh/init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Download install script and run it.
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone git://github.com/robbyrussell/oh-my-zsh.git "${HOME}"/.oh-my-zsh \
+    || { echo "copy .zshrc error"; exit 1; }
 
 # Copy my zsh config file.
 cp _zshrc "${HOME}"/.zshrc || { echo "copy .zshrc error"; exit 1; }
