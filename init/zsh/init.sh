eb2f0c74cb050baf38a99cf5afe8bf72af0e2328
Shengkui Leng <lengshengkui@outlook.com>
2017-06-06 14:29:20 +0800

    add comment to zsh init script

--- init/zsh/init.sh
+++ init/zsh/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Download install script and run it.
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+# Copy my zsh config file.
 cp _zshrc "${HOME}"/.zshrc
