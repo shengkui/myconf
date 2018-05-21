6d12a827defd4f41537da92a192e68d074c28487
Shengkui Leng <lengshengkui@outlook.com>
2018-05-21 15:20:41 +0800

    [os] update package install script

--- init/os/centos_init.sh
+++ init/os/centos_init.sh
@@ -2,12 +2,15 @@
 #Install some software package for CentOS Linux system
 set -u
 
-INST="yum install"
+INST="yum -y install"
 
-SYSTEM="zsh expect tree curl openssl rpcbind net-snmp"
+SYSTEM="zsh expect tree curl openssl rpcbind net-snmp zip unzip zlib unzip
+openssh-server net-tools"
 EDITOR="vim-enhanced vim-X11 cppcheck ctags global"
-DEVELOP="gcc gcc-c++ golang automake autoconf libtool cmake gdb valgrind tcl"
-LIBRARY="libxml2-devel pciutils-devel tcl-devel net-snmp-devel"
+DEVELOP="gcc gcc-c++ golang make automake autoconf libtool cmake gdb valgrind
+tcl kernel-devel kernel-headers glibc glibc-devel glib2 libstdc++"
+LIBRARY="libxml2-devel pciutils-devel tcl-devel net-snmp-devel openssl-devel
+ncurses-devel libtool-ltdl zlib-devel"
 OTHER="git subversion tig python-markdown"
 
 $INST $SYSTEM $DEVELOP $EDITOR $LIBRARY $OTHER
