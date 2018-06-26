8d290d447605e38f69543656f82d8cb2d3fe662a
Shengkui Leng <lengshengkui@outlook.com>
2018-06-26 11:10:52 +0800

    [os]add indent

--- init/os/ubuntu_init.sh
+++ init/os/ubuntu_init.sh
@@ -9,6 +9,6 @@ openssh-server net-tools"
 EDITOR="vim vim-gui-common cppcheck shellcheck ctags global"
 DEVELOP="gcc g++-multilib golang automake autoconf libtool cmake gdb valgrind tcl"
 LIBRARY="libxml2-dev libpci-dev tcl-dev libsnmp-dev libelf-dev"
-OTHER="git subversion tig markdown w3m p7zip"
+OTHER="git subversion tig markdown w3m p7zip indent"
 
 $INST $SYSTEM $DEVELOP $EDITOR $LIBRARY $OTHER
