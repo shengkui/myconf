383363524e8ccf00b27e218227d4ca9a2b981fbb
Shengkui Leng <lengshengkui@outlook.com>
2018-09-27 10:41:52 +0800

    add comment; remove non-used code

--- init/vim/init.sh
+++ init/vim/init.sh
@@ -5,7 +5,8 @@ VIM_DIR="${HOME}/.vim"
 mkdir -p "${VIM_DIR}"/{bundle,tags,undo} || { echo "make .vim dir error"; exit 1; }
 cp -avf UltiSnips "${VIM_DIR}" || { echo "copy UltiSnips error"; exit 1; }
 cp -vf _vimrc "${HOME}"/.vimrc || { echo "copy .vimrc error"; exit 1; }
-#git clone https://github.com/VundleVim/Vundle.vim.git "${VIM_DIR}"/bundle/Vundle.vim
+
+#Install vim-plug
 curl -fLo "${VIM_DIR}"/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
     || { echo "intsll vim-plug error"; exit 1; }
