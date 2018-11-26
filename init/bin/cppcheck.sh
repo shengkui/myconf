8740cf01ed39e1c4291b21eae88350fb2c5268b1
Shengkui Leng <lengshengkui@outlook.com>
2018-11-26 13:55:36 +0800

    modify the option of cppcheck in vimrc and cppcheck.sh to enable more check

--- init/bin/cppcheck.sh
+++ init/bin/cppcheck.sh
@@ -5,6 +5,6 @@ TARGET=.
 if [ $# -eq 1 ];then
     TARGET=$1
 fi
-OPTION="--enable=warning --suppress=nullPointerRedundantCheck"
+OPTION="--enable=style --suppress=nullPointerRedundantCheck --suppress=variableScope --suppress=redundantAssignment"
 
 cppcheck $OPTION $TARGET
