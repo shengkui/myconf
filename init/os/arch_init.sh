30a311bbdfc249d1f98dd20ae5d907ce79415e0f
Shengkui Leng <lengshengkui@outlook.com>
2019-04-15 13:55:10 +0800

    [os]add discount(markdown) to arch init script

--- init/os/arch_init.sh
+++ init/os/arch_init.sh
@@ -8,7 +8,7 @@ SYSTEM="zsh expect tree curl openssl rpcbind net-snmp zip unzip zlib openssh net
 EDITOR="gvim vim-runtime cppcheck shellcheck ctags"
 DEVELOP="gcc gcc-libs clang go make automake autoconf libtool cmake gdb valgrind tcl glibc glib2"
 LIBRARY="libxml2 pciutils ncurses freetype2"
-OTHER="git subversion tig w3m p7zip"
+OTHER="git subversion tig w3m p7zip discount"
 X86LIB="lib32-gcc-libs lib32-glibc"
 
 $INST $SYSTEM $DEVELOP $EDITOR $LIBRARY $OTHER $X86LIB
