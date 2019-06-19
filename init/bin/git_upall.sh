0c7041f951ad28f68f64b05a6e6d970a59d2f021
Shengkui Leng <lengshengkui@outlook.com>
2019-06-19 14:17:37 +0800

    [git_upall]support 2-level directory

--- init/bin/git_upall.sh
+++ init/bin/git_upall.sh
@@ -1,35 +1,58 @@
 #!/bin/bash
+#===============================================================================
+# FILENAME:
+#     git_upall.sh
 #
-# Update all git repositories via "git pull"
-#
+# DESCRIPTION:
+#     Update all git repositories via "git pull"
+#===============================================================================
+set -o nounset                  # Treat unset variables as an error
+set -o pipefail                 # Prevent errors in a pipeline from being masked
 
-for i in *; do
-    #Skip non-directory
-    if [ ! -d "$i" ];then
-        continue
+#Do pull operation for git repo
+pull_repo() {
+    if [ $# -ne 1 ];then
+        echo "invalid parameter"
+        return 1
     fi
-
-    #If the directory is NOT a git repo, skip it.
-    if [ ! -d "$i/.git" ];then
-        continue
-    fi
+    local dir=$1
 
     echo
     echo "-----------------------------------------"
-    echo " updating $i ..."
+    echo " updating $dir ..."
     echo "-----------------------------------------"
     echo
-    pushd . > /dev/null || continue
-    cd "$i" || { echo "Enter dir $i error"; exit 1; }
-    git pull 2>&1 || { echo "Update Git repo error"; exit 1; }
+    cd "$dir" || { echo "Enter dir $dir error"; return 1; }
+
+    git pull 2>&1 || { echo "Update Git repo error"; return 1; }
     if [ -f ".gitmodules" ];then
         git submodule update --init --recursive 2>&1 || \
-            { echo "submodule update error"; exit 1; }
+            { echo "submodule update error"; return 1; }
     fi
+
+    return 0
+}
+
+for i in *; do
+    #If it's a git repo directory, process it.
+    if [[ -d "$i" && -d "$i/.git" ]];then
+        pushd . > /dev/null || continue
+        pull_repo $i
+        popd > /dev/null || continue
+        continue
+    fi
+
+    pushd . > /dev/null || continue
+    cd "$i" || { echo "enter dir $i error"; continue; }
+    for j in *; do
+        #If it's a git repo directory, process it.
+        if [[ -d "$j" && -d "$j/.git" ]];then
+            pushd . > /dev/null || continue
+            pull_repo $j
+            popd > /dev/null || continue
+        fi
+    done
     popd > /dev/null || continue
 done
 
-echo
-echo "done"
-echo
 exit 0
