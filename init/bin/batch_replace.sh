0205708540c64571359ce523f9ccc61784cc1621
Shengkui Leng <lengshengkui@outlook.com>
2019-07-02 10:09:25 +0800

    add script to do batch replace in files

---
+++ init/bin/batch_replace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 3 ];then
+    echo "A script to replace a string in all files under the directory"
+    echo "$0 <directory> <original string> <new string>"
+    exit 1
+fi
+
+TDIR="$1"
+OLD_STR="$2"
+NEW_STR="$3"
+
+echo "replace \"${OLD_STR}\" with \"${NEW_STR}\" in files \"${TDIR}\""
+#Replace a string in files under specified directory
+find ${TDIR} -type f -exec sed -i "s/${OLD_STR}/${NEW_STR}/g" {} \;
