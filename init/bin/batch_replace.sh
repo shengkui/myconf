0e3d46f13a13f73917b77931f2963b4e3d79df64
Shengkui Leng <lengshengkui@outlook.com>
2019-09-06 09:39:52 +0800

    [batch_replace] support delete string

--- init/bin/batch_replace.sh
+++ init/bin/batch_replace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 3 ];then
+if [ $# -lt 2 ] || [ $# -gt 3 ];then
     echo "A script to replace a string in all files under the directory"
     echo "$0 <directory> <original string> <new string>"
     exit 1
@@ -12,4 +12,4 @@ NEW_STR="$3"
 
 echo "replace \"${OLD_STR}\" with \"${NEW_STR}\" in files \"${TDIR}\""
 #Replace a string in files under specified directory
-find ${TDIR} -type f -exec sed -i "s/${OLD_STR}/${NEW_STR}/g" {} \;
+find "${TDIR}" -type f -exec sed -i "s/${OLD_STR}/${NEW_STR}/g" {} \;
