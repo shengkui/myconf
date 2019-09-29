fdc9713743831fe84b267032d0287a6ae7f010bf
Shengkui Leng <lengshengkui@outlook.com>
2019-09-29 15:41:16 +0800

    [os]add ag & html2text

--- init/os/arch_init.sh
+++ init/os/arch_init.sh
@@ -8,7 +8,7 @@ SYSTEM="zsh expect tree curl openssl rpcbind net-snmp zip unzip zlib openssh net
 EDITOR="gvim vim-runtime cppcheck shellcheck ctags"
 DEVELOP="gcc gcc-libs clang go make automake autoconf libtool cmake gdb valgrind tcl glibc glib2"
 LIBRARY="libxml2 pciutils ncurses freetype2"
-OTHER="git subversion tig w3m p7zip discount"
+OTHER="git tig w3m p7zip discount the_silver_searcher python-html2text"
 X86LIB="lib32-gcc-libs lib32-glibc"
 
 $INST $SYSTEM $DEVELOP $EDITOR $LIBRARY $OTHER $X86LIB
