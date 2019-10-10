60a8632718dae21679f15424bc4cf2663040dd60
Shengkui Leng <lengshengkui@outlook.com>
2019-10-10 09:28:54 +0800

    remove dir before install vim/zsh config

--- init/vim/init.sh
+++ init/vim/init.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 VIM_DIR="${HOME}/.vim"
+VIM_CFG="${HOME}/.vimrc"
 
-mkdir -p "${VIM_DIR}"/{bundle,tags,undo} || { echo "make .vim dir error"; exit 1; }
+if [ -d "${VIM_DIR}" ];then
+    rm -rf "${VIM_DIR}"
+fi
+mkdir -p "${VIM_DIR}"/{bundle,tags,undo} ||
+    { echo "create .vim dir error"; exit 1; }
+
 cp -avf UltiSnips "${VIM_DIR}" || { echo "copy UltiSnips error"; exit 1; }
-cp -vf _vimrc "${HOME}"/.vimrc || { echo "copy .vimrc error"; exit 1; }
+cp -vf _vimrc "${VIM_CFG}" || { echo "copy .vimrc error"; exit 1; }
 
 #Install vim-plug
 curl -fLo "${VIM_DIR}"/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
-    || { echo "intsll vim-plug error"; exit 1; }
+    || { echo "install vim-plug error"; exit 1; }
 
-#vim lsp server for Go
-go get -u github.com/sourcegraph/go-langserver
+#Install vim lsp server for Go
+go get -u github.com/sourcegraph/go-langserver ||
+    { echo "install lsp server for go error"; exit 1; }
--- init/zsh/init.sh
+++ init/zsh/init.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+OH_MY_ZSH_DIR="${HOME}/.oh-my-zsh"
+ZSH_CFG="${HOME}/.zshrc"
+ZJUMP_DIR="${OH_MY_ZSH_DIR}"/custom/plugins/zjump
+FZF_DIR="${HOME}"/.fzf
+
+if [ -d "${OH_MY_ZSH_DIR}" ];then
+    rm -rf "${OH_MY_ZSH_DIR}"
+fi
+
 # Download install script and run it.
 #sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone git://github.com/robbyrussell/oh-my-zsh.git "${HOME}"/.oh-my-zsh \
+git clone git://github.com/robbyrussell/oh-my-zsh.git "${OH_MY_ZSH_DIR}" \
     || { echo "download oh-my-zsh error"; exit 1; }
 
 # Copy my zsh config file.
-cp _zshrc "${HOME}"/.zshrc || { echo "copy .zshrc error"; exit 1; }
+cp _zshrc "${ZSH_CFG}" || { echo "copy .zshrc error"; exit 1; }
 
 # Install zjump plugin
-git clone https://github.com/qoomon/zjump "${HOME}"/.oh-my-zsh/custom/plugins/zjump
+git clone https://github.com/qoomon/zjump "${ZJUMP_DIR}"
 
 # Install fzf(needed by ZSH zjump plugin)
-git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}"/.fzf
-"${HOME}"/.fzf/install --bin
-sudo ln -sf "${HOME}"/.fzf/bin/fzf /usr/local/bin/fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git "${FZF_DIR}"
+"${FZF_DIR}"/install --bin
+sudo ln -sf "${FZF_DIR}"/bin/fzf /usr/local/bin/fzf
