1d9c905e8dbd768a4e11937a93802a6f2502493c
Shengkui Leng <lengshengkui@outlook.com>
2019-10-10 17:33:39 +0800

    add argument to cppcheck.sh

--- init/bin/cppcheck.sh
+++ init/bin/cppcheck.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+#Run cppcheck with different checks.
 set -o nounset                              # Treat unset variables as an error
 
 TARGET=.
-if [ $# -eq 1 ];then
-    TARGET=$1
-fi
-OPTION="--enable=style --force --error-exitcode=1" 
+ARG="--enable=warning --force --error-exitcode=1"
+if [ $# -ge 1 ];then
+    while getopts :as opt ;do
+        case $opt in
+            a)  ARG="--enable=all --force --error-exitcode=1"
+                ;;
+            s)  ARG="--enable=style --force --error-exitcode=1"
+                ;;
+            *)  echo "Usage: $0 [-a] [target]"
+                exit 1
+                ;;
+        esac
+    done
 
-cppcheck $OPTION $TARGET
+    shift $((OPTIND-1))
+    if [ $# -ge 1 ];then
+        TARGET=$1
+    fi
+fi
+
+echo "cppcheck $ARG $TARGET"
+cppcheck $ARG $TARGET
