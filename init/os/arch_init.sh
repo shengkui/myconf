cf6d2f40e4c9ca129847bf5179f1b851f77be2bc
Shengkui Leng <lengshengkui@outlook.com>
2020-03-20 09:35:28 +0800

    install fzf under archlinux

--- init/os/arch_init.sh
+++ init/os/arch_init.sh
@@ -8,7 +8,7 @@ SYSTEM="zsh expect tree curl openssl net-snmp zip unzip zlib openssh net-tools"
 EDITOR="gvim vim-runtime cppcheck shellcheck ctags"
 DEVELOP="gcc gcc-libs clang go make automake autoconf libtool cmake gdb valgrind tcl glibc glib2"
 LIBRARY="libxml2 pciutils ncurses freetype2"
-OTHER="git tig lynx p7zip discount the_silver_searcher python-html2text"
+OTHER="git tig lynx p7zip discount the_silver_searcher python-html2text fzf"
 X86LIB="lib32-gcc-libs lib32-glibc"
 
 $INST $SYSTEM $DEVELOP $EDITOR $LIBRARY $OTHER $X86LIB
