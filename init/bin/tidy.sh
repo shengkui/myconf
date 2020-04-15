9d80ad4d5ff2662c0dacd058db04c36ec82c90ea
Shengkui Leng <lengshengkui@outlook.com>
2020-04-15 10:24:28 +0800

    [tidy] use find to get list of all c/cpp files

--- init/bin/tidy.sh
+++ init/bin/tidy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#A simple bash script to run clant-tidy with all c files under current
+#A simple bash script to run clant-tidy with all c/cpp files under current
 #directory.
-#It only works under bash v4+, for it uses a feature only supported by bash v4+.
 
 #Get directory list
 tmp=$(find . -type f -name '*.h' | sed -r 's#/[^/]+$##' | sort | uniq)
@@ -12,5 +11,9 @@ for i in $tmp ;do
     list="$list -I $i"
 done
 
-#Run clang-tidy
-clang-tidy **/*.c -- -Wall -Wextra -O2 $list
+source_file=$(find . -type f \( -name \*.c -o -name \*.cpp \))
+
+for f in $source_file ;do
+    #Run clang-tidy
+    clang-tidy $f -- -Wall -Wextra -O2 $list
+done
