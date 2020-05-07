7648bc6e5efb90f3cb3a3051eace0f862a7eac88
Shengkui Leng <lengshengkui@outlook.com>
2020-05-07 14:14:00 +0800

    add clang-tool-extra

--- init/os/centos_init.sh
+++ init/os/centos_init.sh
@@ -6,7 +6,7 @@ INST="yum -y install"
 
 SYSTEM="zsh expect tree curl openssl net-snmp zip unzip zlib openssh-server openssh-clients net-tools"
 EDITOR="vim-enhanced vim-X11 cppcheck shellcheck ctags global"
-DEVELOP="gcc gcc-c++ clang clang-analyzer golang make automake autoconf libtool cmake gdb valgrind tcl kernel-devel kernel-headers glibc glibc-devel glib2 libstdc++"
+DEVELOP="gcc gcc-c++ clang clang-analyzer clang-tools-extra golang make automake autoconf libtool cmake gdb valgrind tcl kernel-devel kernel-headers glibc glibc-devel glib2 libstdc++"
 LIBRARY="libxml2-devel pciutils-devel tcl-devel net-snmp-devel openssl-devel ncurses-devel libtool-ltdl zlib-devel"
 OTHER="git subversion tig python-markdown lynx p7zip indent ack"
 X86LIB="glibc.i686 glibc-devel.i686 glib2.i686 libstdc++.i686"
