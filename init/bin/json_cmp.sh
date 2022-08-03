32714a963dba9612845ec475b77d9d5a7b81985f
Shengkui Leng <lengshengkui@outlook.com>
2022-08-03 09:06:49 +0800

    add bash script to compare 2 json files

---
+++ init/bin/json_cmp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#===============================================================================
+# FILENAME:
+#     git_upall.sh
+#
+# DESCRIPTION:
+#     A script to compare 2 JSON files.
+#===============================================================================
+set -o nounset                  # Treat unset variables as an error
+set -o pipefail                 # Prevent errors in a pipeline from being masked
+
+if [ $# -ne 2 ] ;then
+    echo "Usage: $0 <file1> <file2>"
+    exit 1
+fi
+
+f1=$1
+f2=$2
+
+compare_by_type() {
+    case $1 in
+        *.json ) diff <(jq . "$1") <(jq . "$2") >&2 ;;
+        * )      diff "$1" "$2" >&2 ;;
+    esac
+    return
+}
+
+if ! compare_by_type "$f1" "$f2"; then
+    echo "files are different."
+    exit 2
+else
+    echo "files are identical."
+    exit 0
+fi
