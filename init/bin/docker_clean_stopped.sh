851da6c578354b949d0ac8c1b4c795b2152e6dc4
Shengkui Leng <lengshengkui@outlook.com>
2022-10-09 15:52:26 +0800

    clean created but not running container

--- init/bin/docker_clean_stopped.sh
+++ init/bin/docker_clean_stopped.sh
@@ -2,3 +2,6 @@
 
 #Remove all stopped docker containers.
 docker rm -f $(docker ps -f "status=exited" -q)
+
+#Remove all created but not running docker containers.
+docker rm -f $(docker ps -f "status=created" -q)
