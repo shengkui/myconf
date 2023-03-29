5360ec8139c436819c81937fff3e3e54df330e4f
Shengkui Leng <lengshengkui@outlook.com>
2023-03-29 16:55:22 +0800

    optmize

--- init/bin/docker_clean_stopped.sh
+++ init/bin/docker_clean_stopped.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #Remove all stopped docker containers.
-docker rm -f $(docker ps -f "status=exited" -q)
+#docker rm -f $(docker ps -f "status=exited" -q)
+docker container prune -f
 
 #Remove all created but not running docker containers.
-docker rm -f $(docker ps -f "status=created" -q)
+created=$(docker ps -f "status=created" -q)
+if [ ! -z "$created" ];then
+    docker rm -f $created
+fi
