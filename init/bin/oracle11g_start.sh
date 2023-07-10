49903d274ea157e9de3559679591a4a9d50411df
Shengkui Leng <lengshengkui@outlook.com>
2023-07-10 09:15:37 +0800

    add restart option for databases

--- init/bin/oracle11g_start.sh
+++ init/bin/oracle11g_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -itd -p 1521:1521 --name oracle11g \
+docker run -itd -p 1521:1521 --name oracle11g --restart=always \
       registry.cn-hangzhou.aliyuncs.com/helowin/oracle_11g
--- init/bin/postgres_start.sh
+++ init/bin/postgres_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker run --name postgresql -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=admin -p 5432:5432 -v /data:/var/lib/postgresql/data -d postgres:bullseye
+docker run --name postgresql -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=admin -p 5432:5432 \
+    --restart=always -v /data:/var/lib/postgresql/data -d postgres:bullseye
 
