9d50881a6edeb1cc229baaabd81bdd50201e7745
Shengkui Leng <lengshengkui@outlook.com>
2023-11-27 09:41:54 +0800

    add sql of DBeaver

---
+++ DBeaver/Scripts/dis_dataset_without_lineage.sql
@@ -0,0 +1,5 @@
+--select dataset.*, org.database_name from dis.dataset inner join dis.org ON org.org_id = dataset.org_id where lineage is null or lineage = '{}'
+--select dataset.*, org.database_name from dis.dataset inner join dis.org ON org.org_id = dataset.org_id where schema is null or schema = null::jsonb
+--SELECT lineage FROM dis.dataset where lineage::text = '{}'::text;
+--select lineage FROM dis.dataset where lineage = '{}'::jsonb;
+SELECT dataset.*, org.database_name FROM dis.dataset inner join dis.org ON org.org_id = dataset.org_id WHERE lineage = '{}'::jsonb
