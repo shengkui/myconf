ccd160757039b81e1fb83d1ccbc4b5f1d83b980a
Shengkui Leng <lengshengkui@outlook.com>
2023-12-04 09:19:45 +0800

    update SQL script of DBeaver

--- DBeaver/Scripts/sqlserver_date.sql
+++ DBeaver/Scripts/sqlserver_date.sql
@@ -11,3 +11,18 @@ SELECT COUNT(*) FROM dbo.sfc_ods_sap_ztmm
 --  WHERE ("CREATE_DATE" IS NOT NULL) AND ("CREATE_DATE" != '00000000') AND (CAST("CREATE_DATE" AS date) < '2023-06-24')
 
 --SELECT CAST("CREATE_DATE" AS date) FROM "dbo"."sfc_ods_sap_ztmm"
+  
+---------------------------------------------------------------  
+--不支持 EXTRACT
+--SELECT EXTRACT(MONTH FROM '2023-11-30')
+
+--支持 DATEPART
+SELECT * FROM "dbo"."it_data"
+  WHERE ("Date" IS NOT NULL) 
+  AND (DATEPART(MONTH, "Date") = 11)
+  AND (DATEPART(YEAR, "Date") = 2023)
+
+SELECT * FROM "dbo"."it_data"
+  WHERE ("Date" IS NOT NULL)
+  AND (DATEPART(MONTH, "Date") = DATEPART(MONTH,GETDATE()))
+  AND (DATEPART(YEAR, "Date") =  DATEPART(YEAR,GETDATE()))
