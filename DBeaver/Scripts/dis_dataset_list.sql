f4b967370d5b95f53eac55ec1c693697236e78bc
Shengkui Leng <lengshengkui@outlook.com>
2024-01-10 15:24:59 +0800

    update dbeaver script

--- DBeaver/Scripts/dis_dataset_list.sql
+++ DBeaver/Scripts/dis_dataset_list.sql
@@ -1,2 +1,2 @@
-select org.database_name, dataset.dataset_name, dataset.description from dataset inner join org on dataset.org_id = org.org_id
+select org.database_name, dataset.dataset_name, dataset.type, dataset.description from dataset inner join org on dataset.org_id = org.org_id
   where org.database_name = 'main'
