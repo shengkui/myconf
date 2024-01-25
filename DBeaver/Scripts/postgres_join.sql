2b0b0fd229d979f4f4ab8261a6b33547c2147a3f
Shengkui Leng <lengshengkui@outlook.com>
2024-01-25 15:04:34 +0800

    [dbeaver] update join sql

--- DBeaver/Scripts/postgres_join.sql
+++ DBeaver/Scripts/postgres_join.sql
@@ -1,5 +1,14 @@
 --Specify columns to avoid duplicated columns in the result.
-SELECT id,firstname,lastname,email,profession,public.citylist."CityName" FROM public.emplist JOIN public.citylist ON public.emplist."CityNo" = public.citylist."CityNo" limit 10
+SELECT id,firstname,lastname,email,profession,public.citylist."CityName" FROM public.emplist
+  JOIN public.citylist ON public.emplist."CityNo" = public.citylist."CityNo"
+  LIMIT 10;
+
+--Avoid duplicated columns in the result.
+SELECT emplist.*, citylist."CityName" FROM emplist 
+  LEFT JOIN citylist ON emplist."CityNo" = citylist."CityNo"
+  LIMIT 10;
 
 --Use table alias to simplify the SQL statement.
-SELECT id,firstname,lastname,email,profession,c."CityName" FROM public.emplist as e RIGHT JOIN public.citylist as c ON e."CityNo" = c."CityNo" limit 10
+SELECT id,firstname,lastname,email,profession,c."CityName" FROM public.emplist as e
+  RIGHT JOIN public.citylist as c ON e."CityNo" = c."CityNo"
+  LIMIT 10;
