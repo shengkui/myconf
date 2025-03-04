cd474f20243e0b1a0185e310c89f5cc1d195ed18
Shengkui Leng <lengshengkui@outlook.com>
2025-03-04 17:50:04 +0800

    add bash script to print GC info of java processes

---
+++ init/bin/gc_info.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cmd_output=`jps | grep -v "Jps"`
+
+declare -a PROCESS_IDS=()
+declare -a PROCESS_NAMES=()
+
+#Save PID and Process Name into array.
+while read -r line; do
+    col1=$(echo "$line" | awk '{print $1}')
+    PROCESS_IDS+=("$col1")
+
+    col2=$(echo "$line" | awk '{print $2}')
+    PROCESS_NAMES+=("$col2")
+done <<< "$cmd_output"
+
+count=${#PROCESS_NAMES[@]}
+
+n=0
+for proc_id in "${PROCESS_IDS[@]}" ;do
+    echo "=========== ${proc_id} ${PROCESS_NAMES[$n]}"
+    jstat -gcutil ${proc_id}
+
+    ((n++))
+    if [ $n -ge $count ];then
+        n=0
+    fi
+    echo
+done
