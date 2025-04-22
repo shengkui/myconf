f4daf4e1d204228a48ae808802c2dafd8d30a7e4
Shengkui Leng <lengshengkui@outlook.com>
2025-04-22 11:33:11 +0800

    remove unnecessary env variable

--- init/bin/hive_metastore.sh
+++ init/bin/hive_metastore.sh
@@ -1,12 +1,10 @@
-export JAVA_HOME=/usr/lib/jvm/java/
+#export JAVA_HOME=/usr/lib/jvm/java/
 
-export HADOOP_HOME=/home/shengkui.leng/hadoop
-export HADOOP_CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath`
-export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$HADOOP_HOME/share/hadoop/tools/lib/*
+#export HADOOP_HOME=/home/shengkui.leng/hadoop
+#export HADOOP_CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath`
+#export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$HADOOP_HOME/share/hadoop/tools/lib/*
 
-export HIVE_HOME=/home/shengkui.leng/hive
-
-export PATH=$PATH:/home/shengkui.leng/flink/bin
+#export HIVE_HOME=/home/shengkui.leng/hive
 
 cd $HIVE_HOME
 nohup bin/hive --service metastore &
